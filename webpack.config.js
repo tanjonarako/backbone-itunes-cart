3310cffc9af6be73ffa738d8394dc6f579adb783
Tanjona Rakotoarisoa <tanjona@stormz.me>
2017-11-28 03:27:31 +0100

    Add babel-polyfill to fix error

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,21 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: ["babel-polyfill", "./src/index.js"]
     output: {
         path: __dirname,
         filename: "./build/bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.js$/, loader: "babel-loader" },
+            { test: /\.js$/, exclude: /(node_modules)/, loader: "babel-loader" },
             { test: /\.ejs$/, loader: "ejs-loader" },
             { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" },
             { test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: "url-loader" }
         ]
     },
+    devtool: 'source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Backbone itunes cart',
